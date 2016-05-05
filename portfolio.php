a636271f9bbff8043185eea66da019c74e4c7743
Masha Vainblat <MashaVainblat@Mashas-MacBook-Pro.local>
2016-05-05 01:30:37 -0400

    styling adding projects

--- portfolio.php
+++ portfolio.php
@@ -12,147 +12,164 @@
 
                         <div class="portfolioButtons col-md-4 col-sm-4 col-xs-10 projectsColumnM"><!--M COLUMN-->
 
-                              <div class="projectsColumnContents"><!-- to center-->
+                          <div class="projectsColumnContents"><!-- to center-->
 
-                              	<div class="row portfolioProjectsRow">
+                          	<div class="row portfolioProjectsRow">
 
-                              		<div class="col-md-7 col-sm-7 col-xs-7">
-                              			<ul>
-                              				<li>
-                              					<a class="navButton sonnyRollinsButton" href="01.sonnyRollins.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/sonnyRollins.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a>
-                              					<span class="pageName" id="sonnyRollins"><p>sonny rollins</p></span>
-                              				</li>
-                              			</ul>
-                              		</div>
+                          		<div class="col-md-7 col-sm-7 col-xs-7">
+                          			<ul>
+                          				<li>
+                          					<a class="navButton sonnyRollinsButton" href="01.sonnyRollins.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/sonnyRollins.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a>
+                          					<span class="pageName" id="sonnyRollins"><p>sonny rollins</p></span>
+                          				</li>
+                          			</ul>
+                          		</div>
 
-                              		<div class="col-md-5 col-sm-5 col-xs-5">
-                              			<ul>
-                              				<li>
-                              					<a class="navButton kinitoButton" href="02.kinito.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/kinitoCopy.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="kinito"><p>kinito</p></span>
-                              				</li>
-                              			</ul>
-                              		</div>
-                              	</div> <!-- end of row -->
+                          		<div class="col-md-5 col-sm-5 col-xs-5">
+                          			<ul>
+                          				<li>
+                          					<a class="navButton kinitoButton" href="02.kinito.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/kinitoCopy.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="kinito"><p>kinito</p></span>
+                          				</li>
+                          			</ul>
+                          		</div>
+                          	</div> <!-- end of row -->
 
-                              	<div class="row portfolioProjectsRow">
-                              		<div class="col-md-2 col-sm-2 col-xs-2"></div>
+                          	<div class="row portfolioProjectsRow">
+                          		<div class="col-md-2 col-sm-2 col-xs-2"></div>
 
-                              		<div class="col-md-10 col-sm-10 col-xs-10">
-                              			<ul>
-                              				<li>
-                              					<a class="navButton deCeccoButton" href="05.deCecco.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/deCecco.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="deCecco"><p>de cecco</p></span>
-                              				</li>
-                              			</ul>
-                              		</div>
-                              	</div>
+                          		<div class="col-md-10 col-sm-10 col-xs-10">
+                          			<ul>
+                          				<li>
+                          					<a class="navButton deCeccoButton" href="05.deCecco.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/deCecco.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="deCecco"><p>de cecco</p></span>
+                          				</li>
+                          			</ul>
+                          		</div>
+                          	</div>
 
-                              	<div class="row portfolioProjectsRow"> <!--empty row-->
-                              	</div>
+                          	<div class="row portfolioProjectsRow"> <!--empty row-->
+                              <div class="col-md-7 col-sm-7 col-xs-7"></div> <!-- SPACER -->
+                              <div class="col-md-5 col-sm-5 col-xs-5">
+                                <ul>
+                                  <li>
+                                    <a class="navButton deCeccoButton" href="05.deCecco.php" id="thisProject2">
+                                      <img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/deCecco.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/>
+                                    </a> 
+                                    <span class="pageName" id="deCecco"><p id="thisProjectTitle2">de cecco</p></span>
+                                  </li>
+                                </ul>
+                              </div>
+                          	</div>
 
-                              	<div class="row portfolioProjectsRow">
-                              		<div class="col-md-3 col-sm-3 col-xs-3"></div><!--SPACER-->
+                          	<div class="row portfolioProjectsRow">
+                          		<div class="col-md-3 col-sm-3 col-xs-3">
+                                <ul>
+                                  <li>
+                                    <a class="navButton vosgesButton" href="07.vosges.php" id="thisProject">
+                                      <img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/vosges.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/>
+                                    </a> 
+                                    <span class="pageName" id="vosges"><p id="thisProjectTitle">vosges</p></span>
+                                  </li>
+                                </ul>  
+                              </div><!--SPACER-->
 
-                              		<div class="col-md-9 col-sm-9 col-xs-9">
-                              			<ul>
-                              				<li>
-                              					<a class="navButton vosgesButton" href="07.vosges.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/vosges.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="vosges"><p>vosges</p></span>
-                              				</li>
-                              			</ul>
-                              		</div>
-                              	</div>
+                          		<div class="col-md-9 col-sm-9 col-xs-9">
+                          			<ul>
+                          				<li>
+                          					<a class="navButton vosgesButton" href="07.vosges.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/vosges.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="vosges"><p>vosges</p></span>
+                          				</li>
+                          			</ul>
+                          		</div>
+                          	</div>
 
-                              	<div class="row"></div><!--EMPTY-->
+                          	<div class="row"></div><!--EMPTY-->
 
-                              	<div class="row portfolioProjectsRow">
-                              		<div class="col-md-7 col-sm-7 col-xs-7">
-                              			<ul>
-                              				<li>
-                              					<a class="navButton sweetlyLyalyaButton" href="09.sweetlyLyalya.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/sweetlyLyalyaCopy.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="sweetlyLyalya"><p> sweetly lyalya</p></span>
-                              				</li>
-                              			</ul>
-                              		</div>
+                          	<div class="row portfolioProjectsRow">
+                          		<div class="col-md-7 col-sm-7 col-xs-7">
+                          			<ul>
+                          				<li>
+                          					<a class="navButton sweetlyLyalyaButton" href="09.sweetlyLyalya.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/sweetlyLyalyaCopy.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="sweetlyLyalya"><p> sweetly lyalya</p></span>
+                          				</li>
+                          			</ul>
+                          		</div>
 
-                              		<div class="col-md-5 col-sm-5 col-xs-5">
-                              			<ul>
-                              				<li>
-                              					<a class="navButton anatoliaButton" href="10.anatolia.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/anatoliaCopy.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="anatolia"><p> anatolia</p></span>
-                              				</li>
-                              			</ul>
-                              		</div>
-                              	</div>
-                              </div> <!-- end of centering contents-->
+                          		<div class="col-md-5 col-sm-5 col-xs-5">
+                          			<ul>
+                          				<li>
+                          					<a class="navButton anatoliaButton" href="10.anatolia.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/anatoliaCopy.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="anatolia"><p> anatolia</p></span>
+                          				</li>
+                          			</ul>
+                          		</div>
+                          	</div>
+                          </div> <!-- end of centering contents-->
                         </div> <!--end of projectsColumnM-->
-
-
-
                         
                         <div class="col-md-3 col-sm-3 col-xs-2"></div>
 
                         <div class="portfolioButtons col-md-4 col-sm-4 col-xs-10 projectsColumnV"><!--V COLUMN-->
 
-                              <div class="projectsColumnContents"><!-- to center-->
+                          <div class="projectsColumnContents"><!-- to center-->
 
-                                    <div class="row portfolioProjectsRow">
+                                <div class="row portfolioProjectsRow">
 
-                              		<div class="col-md-5 col-sm-5 col-xs-6">
-                              			<ul>
-                              				<li>
-                              					<a class="navButton neetButton portfolioProjectsRow" href="03.neet.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/neetCopy.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="neet"><p> neet'</p></span>
-                              				</li>
-                              			</ul>
-                              		</div>
+                          		<div class="col-md-5 col-sm-5 col-xs-6">
+                          			<ul>
+                          				<li>
+                          					<a class="navButton neetButton portfolioProjectsRow" href="03.neet.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/neetCopy.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="neet"><p> neet'</p></span>
+                          				</li>
+                          			</ul>
+                          		</div>
 
-                              		<div class="col-md-7 col-sm-7 col-xs-6">
-                              			<ul>
-                              				<li>
-                              					<a class="navButton moMathButton" href="04.moMath.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/moMathCopy.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="moMath"><p> momath museum</p> </span>
-                              				</li>
-                              			</ul>
-                              		</div>
-                              	</div>
+                          		<div class="col-md-7 col-sm-7 col-xs-6">
+                          			<ul>
+                          				<li>
+                          					<a class="navButton moMathButton" href="04.moMath.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/moMathCopy.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="moMath"><p> momath museum</p> </span>
+                          				</li>
+                          			</ul>
+                          		</div>
+                          	</div>
 
-                              	<div class="row portfolioProjectsRow">
-                              		<div class="col-md-1 col-sm-1 col-xs-1"></div><!--empty-->
+                          	<div class="row portfolioProjectsRow">
+                          		<div class="col-md-1 col-sm-1 col-xs-1"></div><!--empty-->
 
-                              		<div class="col-md-10 col-sm-10 col-xs-10">
-                              			<ul>
-                              				<li>
-                              					<a class="navButton sweetBotanicalsButton" href="06.sweetBotanicals.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/sweetBotanicals.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="sweetBotanicals"><p> sweet botanicals</p> </span>
-                              				</li>
-                              			</ul>
-                              		</div>
-                              	</div>
+                          		<div class="col-md-10 col-sm-10 col-xs-10">
+                          			<ul>
+                          				<li>
+                          					<a class="navButton sweetBotanicalsButton" href="06.sweetBotanicals.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/sweetBotanicals.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="sweetBotanicals"><p> sweet botanicals</p> </span>
+                          				</li>
+                          			</ul>
+                          		</div>
+                          	</div>
 
-                              	<div class="row"></div><!--EMPTY-->
+                          	<div class="row"></div><!--EMPTY-->
 
-                              	<div class="row portfolioProjectsRow">
-                              		<div class="col-md-4 col-sm-4 col-xs-4"></div><!--empty-->
+                          	<div class="row portfolioProjectsRow">
+                          		<div class="col-md-4 col-sm-4 col-xs-4"></div><!--empty-->
 
-                              		<div class="col-md-8 col-sm-8 col-xs-8">
-                              			<ul>
-                              				<li>
-                              					<a class="navButton bonniesJamsButton" href="08.bonniesJams.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/bonniesJams.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="bonniesJams"><p> bonnie's jams</p></span>
-                              				</li>
-                              			</ul>
-                              		</div>
-                              	</div>
+                          		<div class="col-md-8 col-sm-8 col-xs-8">
+                          			<ul>
+                          				<li>
+                          					<a class="navButton bonniesJamsButton" href="08.bonniesJams.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/bonniesJams.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="bonniesJams"><p> bonnie's jams</p></span>
+                          				</li>
+                          			</ul>
+                          		</div>
+                          	</div>
 
-                                    <div class="row portfolioProjectsRow"> <!--empty row-->
-                                    </div>
+                                <div class="row portfolioProjectsRow"> <!--empty row-->
+                                </div>
 
-                              	<div class="row"></div><!--EMPTY-->
+                          	<div class="row"></div><!--EMPTY-->
 
-                              	<div class="row portfolioProjectsRow">
-                              		<div class="col-md-3 col-sm-3 col-xs-3"></div>
-                              		<div class="col-md-9 col-sm-9 col-xs-9">
-                              			<ul>
-                              				<li>
-                              					<a class="navButton vOButton" href="11.visualOpinion.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/visualOpinionCopy.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="visualOpinion"><p> visual opinion</p></span>
-                              				</li>
-                              			</ul>
-                              		</div>
-                              	</div>
-                              </div>
+                          	<div class="row portfolioProjectsRow">
+                          		<div class="col-md-3 col-sm-3 col-xs-3"></div>
+                          		<div class="col-md-9 col-sm-9 col-xs-9">
+                          			<ul>
+                          				<li>
+                          					<a class="navButton vOButton" href="11.visualOpinion.php"><img src="img/thumbnails/whiteCircle.png" onmouseover="this.src='img/thumbnails/visualOpinionCopy.png'" onmouseout="this.src='img/thumbnails/whiteCircle.png'"/></a> <span class="pageName" id="visualOpinion"><p> visual opinion</p></span>
+                          				</li>
+                          			</ul>
+                          		</div>
+                          	</div>
+                          </div>
 
                         </div><!--end of V Column-->
 
