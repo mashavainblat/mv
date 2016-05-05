913681d68702663fdf15c0be516eb46a9cd8f9cb
Masha Vainblat <MashaVainblat@Mashas-MacBook-Pro.local>
2016-05-05 10:42:43 -0400

    adding photos to VO

--- 11.visualOpinion.php
+++ 11.visualOpinion.php
@@ -23,10 +23,53 @@
 
                     <div id="imageContainerWidthNoPadding" class="col-md-7 col-sm-7 col-xs-10 noPadding">
                       <div class="row whiteBackgroundColor" id="imagineryLinesPostcardsContainer">
-                        <img src="img/11.visualOpinion/VOPostcards003.jpg">
-                        <img src="img/11.visualOpinion/VOPostcards001.jpg">
-                        <img src="img/11.visualOpinion/VOPostcards002.jpg">
-                        <img src="img/11.visualOpinion/VOPostcards004.jpg">
+                        
+                      <section id="dissension">
+                        <img src="img/11.visualOpinion/dissension/VainblatMashaVODissensionCover01.jpg">
+                        <img src="img/11.visualOpinion/dissension/VainblatMashaVODissension_00.jpg">
+                        <img src="img/11.visualOpinion/dissension/VainblatMashaVODissension_10-11.jpg">
+                        <img src="img/11.visualOpinion/dissension/VainblatMashaVODissension_12-13.jpg">
+                        <img src="img/11.visualOpinion/dissension/VainblatMashaVODissension_16-17.jpg">
+                        <img src="img/11.visualOpinion/dissension/VainblatMashaVODissension_20-21.jpg">
+                        <img src="img/11.visualOpinion/dissension/VainblatMashaVODissension_22-23.jpg">
+                        <img src="img/11.visualOpinion/dissension/VainblatMashaVODissension_26-27.jpg">
+                        <img src="img/11.visualOpinion/dissension/VainblatMashaVODissension_28-29.jpg">
+                        <img src="img/11.visualOpinion/dissension/VainblatMashaVODissension_30-31.jpg">
+                        <img src="img/11.visualOpinion/dissension/VainblatMashaVODissension_38-39.jpg">
+                        <img src="img/11.visualOpinion/dissension/VainblatMashaVODissension_48-49.jpg">
+                        <img src="img/11.visualOpinion/dissension/VainblatMashaVODissension_000.jpg">
+                      </section>
+                      <section id="transformation">
+                        <img src="img/11.visualOpinion/transformation/VainblatMashaVOTransformationCover.jpg">
+                        <img src="img/11.visualOpinion/transformation/VainblatMashaVOTransformation_08-09.jpg">
+                        <img src="img/11.visualOpinion/transformation/VainblatMashaVOTransformation_10-11.jpg">
+                        <img src="img/11.visualOpinion/transformation/VainblatMashaVOTransformation_16-17.2.jpg">
+                        <img src="img/11.visualOpinion/transformation/VainblatMashaVOTransformation_28-29.jpg">
+                        <img src="img/11.visualOpinion/transformation/VainblatMashaVOTransformation_32-33.2.jpg">
+                        <img src="img/11.visualOpinion/transformation/VainblatMashaVOTransformation_34-35.jpg">
+                        <img src="img/11.visualOpinion/transformation/VainblatMashaVOTransformation_36-37.2.jpg">
+                        <img src="img/11.visualOpinion/transformation/VainblatMashaVOTransformation_38-39.jpg">
+                      </section>
+                      <section id="open">
+                        <img src="img/11.visualOpinion/open/VainblatMashaVOOpenCover01.jpg">
+                        <img src="img/11.visualOpinion/open/VO_OPEN_2-3.jpg">
+                        <img src="img/11.visualOpinion/open/VO_OPEN_8-9.jpg">
+                        <img src="img/11.visualOpinion/open/VO_OPEN_14-15.jpg">
+                        <img src="img/11.visualOpinion/open/VO_OPEN_18-19.jpg">
+                        <img src="img/11.visualOpinion/open/VO_OPEN_20-21.jpg">
+                        <img src="img/11.visualOpinion/open/VO_OPEN_26-27.jpg">
+                        <img src="img/11.visualOpinion/open/VO_OPEN_28-29.jpg">
+                        <img src="img/11.visualOpinion/open/VO_OPEN_30-31.jpg">
+                        <img src="img/11.visualOpinion/open/VO_OPEN_40-41.jpg">
+                        <img src="img/11.visualOpinion/open/VO_OPEN_58-59.jpg">
+                        <img src="img/11.visualOpinion/open/VO_OPEN_64-65.jpg">
+                        <img src="img/11.visualOpinion/open/VO_OPEN_68-69.jpg">
+                      </section>
+                        <section id="imagineryLines">
+                          <img src="img/11.visualOpinion/VOPostcards003.jpg">
+                          <img src="img/11.visualOpinion/VOPostcards001.jpg">
+                          <img src="img/11.visualOpinion/VOPostcards002.jpg">
+                          <img src="img/11.visualOpinion/VOPostcards004.jpg">
                       </div>
                     </div>
 
