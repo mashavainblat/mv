ce8c360ced834d6fdcd9f4c581209363b3173fec
Masha Vainblat <mashavainblat@gmail.com>
2016-07-07 18:43:52 -0400

    adding illustrator wireframes

--- includes/navigation.php
+++ includes/navigation.php
@@ -10,28 +10,28 @@
 </div> -->
 
 <nav class="row">
-	<div class="col-sm-2 col-xs-2"><!-- EMPTY --></div>
+	<div class="col-sm-2"><!-- EMPTY --></div>
 
-	<div class="col-sm-2 col-xs-2 navButtonContainer" id="homeButton">
+	<div class="col-sm-2 col-xs-3 navButtonContainer" id="homeButton">
 		<a class="navButton" href="portfolio.php"></a>
 		<p class="buttonName noMargin">portfolio</p>
 	</div>
 
-	<div class="col-sm-2 col-xs-2 navButtonContainer" id="resumeButton">
+	<div class="col-sm-2 col-xs-3 navButtonContainer" id="resumeButton">
 		<a class="navButton" href="resume.php"></a>
 		<p class="buttonName noMargin">resume</p>
 	</div>
 
-	<div class="col-sm-2 col-xs-2 navButtonContainer" id="aboutButton">
+	<div class="col-sm-2 col-xs-3 navButtonContainer" id="aboutButton">
 		<a class="navButton" href="about.php"></a>
 		<p class="buttonName noMargin">about</p>
 	</div>
-	<div class="col-sm-2 col-xs-2 navButtonContainer" id="contactButton">
+	<div class="col-sm-2 col-xs-3 navButtonContainer" id="contactButton">
 		<a class="navButton" href="contact.php"></a>
 		<p class="buttonName fadeIn noMargin">contact</p>
 	</div>
 
-	<div class="col-sm-2 col-xs-2"><!-- EMPTY --></div>
+	<div class="col-sm-2"><!-- EMPTY --></div>
 </nav>
 
 
--- portfolio.php
+++ portfolio.php
@@ -7,19 +7,57 @@
 	<h1>portfolio page</h1>
 	<?php include("includes/navigation.php"); ?>
 
-<section class="row">
-	<div class="col-sm-2 col-xs-2"><!-- EMPTY --></div>
-	<div class="col-sm-2 col-xs-2 projectButtonContainer">
-		<a class="projectButton" href="#"></a>
-		<p class="buttonName noMargin">First Project</p>
-	</div>
-	<div class="col-sm-4 col-xs-4"></div>
-	<div class="col-sm-2 col-xs-2 projectButtonContainer">
-		<a class="projectButton" href="#"></a>
-		<p class="buttonName noMargin">Another Project</p>
-	</div>
-	<div class="col-sm-2 col-xs-2"><!-- EMPTY --></div>
-</section>
+
+
+<main class="row noMargin">
+	<!-- begin M column section -->
+	<section id="M">
+		<div class="col-xs-1"><!-- EMPTY --></div>
+		<div class="col-xs-2 projectButtonContainer">
+			<a class="projectButton" href="#"></a>
+			<p class="buttonName noMargin">Project</p>
+		</div>
+		<div class="col-xs-6"><!-- EMPTY --></div>
+		<div class="col-xs-2 projectButtonContainer">
+			<a class="projectButton" href="#"></a>
+			<p class="buttonName noMargin">Project</p>
+		</div>
+		<div class="col-xs-1"><!-- EMPTY --></div>
+	</section>
+	<!-- end of M column section -->
+
+	<!-- begin V column section -->
+	<section id="V">
+	</section>
+	<!-- end of V column section -->
+</main>
+
+
+
+
+<main>
+	<section class="row noMargin">
+		<div class="col-sm-2"><!-- EMPTY --></div>
+		<div class="col-sm-2 col-xs-2 projectButtonContainer">
+			<a class="projectButton" href="#"></a>
+			<p class="buttonName noMargin">First Project</p>
+		</div>
+		<div class="col-sm-4 col-xs-8"></div>
+		<div class="col-sm-2 col-xs-2 projectButtonContainer">
+			<a class="projectButton" href="#"></a>
+			<p class="buttonName noMargin">Second Project</p>
+		</div>
+		<div class="col-sm-2 col-xs-2"><!-- EMPTY --></div>
+	</section>
+
+	<section class="row noMargin">
+		<div class="col-xs-2"><!-- EMPTY --></div>
+		<div class="col-xs-2 projectButtonContainer">
+			<a class="projectButton" href="#"></a>
+			<p class="buttonName noMargin">3rd Project</p>
+		</div>
+	</section>
+</main>
 
 </body>
 
