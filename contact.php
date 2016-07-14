bf299673fe7ee03bd11995dc5e045a895b1baeae
Masha Vainblat <mashavainblat@gmail.com>
2016-07-14 01:55:17 -0400

    contact page and some styling

--- contact.php
+++ contact.php
@@ -4,5 +4,18 @@
 
 <body id="contact" class="container">
 
-	<h1>Contact page</h1>
 	<?php include("includes/navigation.php"); ?>
+
+	<main class="row">
+
+		<section class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<p>Masha S. Vainblat</p>
+			<a href="https://www.linkedin.com/in/mashavainblat" target="_blank"><img src="/img/contact/linkedinWhite30px.png"></a>
+			<a href="mailto:mashavainblat@gmail.com"><img src="/img/contact/gmailWhite30px.png"></a>
+			<a href="https://github.com/mashavainblat" target="_blank"><img src="/img/contact/githubWhite30px.png"></a>
+		</section>
+	
+	</main>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,7 +5,37 @@
 <body id="index" class="container">
 
 	<h1>Index page</h1>
-	<?php include("includes/navigation.php"); ?>
+	<!-- <?php #include("includes/navigation.php"); ?> -->
+
+
+
+
+	<nav class="row">
+		<div class="col-md-2"><!-- EMPTY --></div>
+
+		<div class="col-xs-12 col-md-2 navButtonContainer" id="homeButton">
+			<a class="navButton" href="portfolio.php"></a>
+			<p class="buttonName noMargin" id="portfolioButtonName">portfolio</p>
+		</div>
+
+		<div class="col-xs-12 col-md-2 navButtonContainer" id="resumeButton">
+			<a class="navButton" href="resume.php"></a>
+			<p class="buttonName noMargin" id="resumeButtonName">resume</p>
+		</div>
+
+		<div class="col-xs-12 col-md-2 navButtonContainer" id="aboutButton">
+			<a class="navButton" href="about.php"></a>
+			<p class="buttonName noMargin" id="aboutButtonName">about</p>
+		</div>
+		<div class="col-xs-12 col-md-2 navButtonContainer" id="contactButton">
+			<a class="navButton" href="contact.php"></a>
+			<p class="buttonName noMargin" id="contactButtonName">contact</p>
+		</div>
+
+		<div class="col-xs-2 col-md-2"><!-- EMPTY --></div>
+	</nav>
+
+
 
 
 
