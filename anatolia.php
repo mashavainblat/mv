5becf02886a1866708122b1b7e6fa359222995aa
Masha Vainblat <mashavainblat@gmail.com>
2016-07-17 18:27:18 -0400

    adding anatolia project

--- anatolia.php
+++ anatolia.php
@@ -103,6 +103,114 @@
 					<img src="img/projects/anatolia/anatolia015bottle.jpg">
 				</div>
 			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/anatolia/anatolia016bottleRack.jpg">
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/anatolia/anatolia017bottleRack.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/anatolia/anatolia018corkScrew.jpg">
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/anatolia/anatolia019glasses.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/anatolia/anatolia020window.jpg">
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/anatolia/anatolia021display.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/anatolia/anatolia022barrels.jpg">
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/anatolia/anatolia023barrels.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 noPadding">
+					<img src="img/projects/anatolia/anatolia024vineyard.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 noPadding">
+					<img src="img/projects/anatolia/anatolia025vineyard.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row projectImgPadding">
+				<div class="col-xs-12 col-sm-7 col-md-7 noPadding">
+					<img src="img/projects/anatolia/anatolia026picker.jpg">
+				</div>
+				<div class="col-xs-12 col-sm-5 col-md-5 noPadding">
+					<img src="img/projects/anatolia/anatolia027picker.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row projectImgPadding">
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/anatolia/anatolia028picker.jpg">
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/anatolia/anatolia029grapes.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row projectImgPadding">
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/anatolia/anatolia030websiteDesktop.jpg">
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/anatolia/anatolia031ad.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row projectImgPadding">
+				<div class="col-xs-12 col-sm-5 col-md-5 noPadding">
+					<img src="img/projects/anatolia/anatolia032ad.jpg">
+				</div>
+				<div class="col-xs-12 col-sm-7 col-md-7 noPadding">
+					<img src="img/projects/anatolia/anatolia033tablet.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-3 noPadding">
+					<img src="img/projects/anatolia/anatolia034shirt.jpg">
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-3 noPadding">
+					<img src="img/projects/anatolia/anatolia035tote.jpg">
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-6 noPadding">
+					<img src="img/projects/anatolia/anatolia036mobile.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
 
 		</section><!-- END OF .images -->
 	</main> <!-- END OF .projectImages row -->
