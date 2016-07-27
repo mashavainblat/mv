1b3d0539d3ea0dd03c73cab0e4514d701ec301a4
Masha Vainblat <mashavainblat@gmail.com>
2016-07-27 01:01:20 -0400

    adding Hex project

---
+++ hex.php
@@ -0,0 +1,61 @@
+<?php include("includes/formattop.php"); ?>
+	<title>50 Shades of Hex</title>
+</head>
+
+<body id="hex" class="container project">
+	<?php include("includes/navigation.php"); ?>
+
+	<main class="row projectTitleDesc">
+	
+		<section class="col-xs-1 col-sm-1 col-md-3 noPadding"></section>
+		<section class="col-xs-10 col-sm-10 col-md-6 noPadding">
+
+			<h3 class="projectTitle noMargin">50 Shades of Hex</h3>
+			<h4 class="projectCategory noMargin">Web development</h4>
+			<p class="projectDescription">50 Shades of Hex is a website that allows designers to create, share and save color palettes based on hex code. Now there is a one stop shop for every designers favorite color palettes.</p> 
+			<p class="projectDescription">This is an express web application built with Express.js and Node.js</p>
+
+		</section>
+		<section class="col-xs-1 col-sm-1 col-md-3 noPadding"></section>
+	</main>
+
+	<main class="row projectImages">
+
+		<section class="col-md-2 hideThis"></section>
+		<section class="col-xs-12 col-md-8 noPadding images">
+
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 noPadding">
+					<img src="img/projects/hex/hex001.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 noPadding">
+					<img src="img/projects/hex/hex002.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 noPadding">
+					<img src="img/projects/hex/hex003.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 noPadding" id="hexMobile">
+					<img src="img/projects/hex/hex004.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+		</section><!-- END OF .images -->
+		<section class="col-md-2 hideThis"></section>
+
+	</main>
+
+</body>
+</html>	
