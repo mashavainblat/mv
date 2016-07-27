2c7573ff816a116d35f15d55458d4e730b45d04f
Masha Vainblat <mashavainblat@gmail.com>
2016-07-27 01:04:19 -0400

    adding Nag Me page

---
+++ nagMe.php
@@ -0,0 +1,55 @@
+<?php include("includes/formattop.php"); ?>
+	<title>Nag Me</title>
+</head>
+
+<body id="nagMe" class="container project">
+	<?php include("includes/navigation.php"); ?>
+
+	<main class="row projectTitleDesc">
+	
+		<section class="col-xs-1 col-sm-1 col-md-3 noPadding"></section>
+		<section class="col-xs-10 col-sm-10 col-md-6 noPadding">
+
+			<h3 class="projectTitle noMargin">Nag Me</h3>
+			<h4 class="projectCategory noMargin">Web development</h4>
+			<p class="projectDescription">Nag Me is a to do list that will 'nag' users to finish a task until it is complete. This is an express web application built with Express.js and Node.js</p>
+
+		</section>
+		<section class="col-xs-1 col-sm-1 col-md-3 noPadding"></section>
+	</main>
+
+	<main class="row projectImages">
+
+		<section class="col-md-2 hideThis"></section>
+		<section class="col-xs-12 col-md-8 noPadding images">
+
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 noPadding">
+					<img src="img/projects/nagMe/nagMe001.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 col-sm-12 col-md-12 noPadding">
+					<img src="img/projects/nagMe/nagMe002.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/nagMe/nagMe003.jpg">
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/nagMe/nagMe004.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+		</section> <!-- END OF .images -->
+		<section class="col-md-2 hideThis"></section>
+
+	</main>
+</body>
+</html>
