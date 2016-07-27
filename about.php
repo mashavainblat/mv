8d545b6418aa3c06e9f395daa101ef6b4d39f132
Masha Vainblat <mashavainblat@gmail.com>
2016-07-27 00:49:52 -0400

    adding about img and description

--- about.php
+++ about.php
@@ -3,6 +3,33 @@
 </head>
 
 <body id="about" class="container">
-
-	<h1>About page</h1>
 	<?php include("includes/navigation.php"); ?>
+
+	<main class="row projectTitleDesc">
+		<section class="col-xs-1 col-sm-1 col-md-3 noPadding"></section>
+		<section class="col-xs-10 col-sm-10 col-md-6 noPadding">
+
+			<h3 class="projectTitle noMargin">About Masha</h3>
+			<p class="projectDescription">A recent graduate from the School of Visual Arts, Masha Vainblat is a web designer/developer constantly looking for new creative challenges. Her passion and persistence to be a well rounded designer have led her to learn full stack web development at General Assembly's immersive bootcamp. With a strong foundation in the fundamentals of art and design, she has an array of skills and looks forward to exercising them while constantly learning new technologies.</p>
+
+		</section>
+		<section class="col-xs-1 col-sm-1 col-md-3 noPadding"></section>
+
+	</main>
+	<main class="row projectImages">
+
+		<section class="col-md-2 hideThis"></section>
+		<section class="col-xs-12 col-md-8 noPadding images">
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 noPadding">
+					<img src="img/about/about.gif">
+				</div>
+			</div>
+			<!-- IMG -->
+		</section>
+		<section class="col-md-2 hideThis"></section>
+
+	</main>
+
+</body>
