96c22e0f4d66714ae933202f5cad6b7cc80d7e93
Masha Vainblat <mashavainblat@gmail.com>
2016-07-27 01:18:58 -0400

    updating nav include file and updating JS file so nav is transparent on scroll

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,24 +1,37 @@
 
 
-<nav>
-	<div class="col-xs-2"></div>
-	<div class="col-xs-2 navButtonContainer" id="homeButton">
+
+
+
+
+
+
+
+
+<nav class="row navigation">
+	<div class="col-xs-2 col-md-2"></div>
+
+	<div class="col-xs-2 navButtonContainer" id="portfolioButton">
 		<a class="navButton" href="portfolio.php"></a>
 		<a class="buttonName noMargin" href="portfolio.php" id="portfolioButtonName">portfolio</a>
 	</div>
+
 	<div class="col-xs-2 navButtonContainer" id="resumeButton">
 		<a class="navButton" href="resume.php"></a>
 		<a class="buttonName noMargin" href="resume.php" id="resumeButtonName">resume</a>
 	</div>
+
 	<div class="col-xs-2 navButtonContainer" id="aboutButton">
 		<a class="navButton" href="about.php"></a>
 		<a class="buttonName noMargin" href="about.php" id="aboutButtonName">about</a>
 	</div>
+
 	<div class="col-xs-2 navButtonContainer" id="contactButton">
 		<a class="navButton" href="contact.php"></a>
 		<a class="buttonName noMargin" href="contact.php" id="contactButtonName">contact</a>
 	</div>
-	<div class="col-xs-2"></div>
+	
+	<div class="col-xs-2 col-md-2"></div>
 </nav>
 
 <br clear="all" id="navBreak">
--- scripts/app.js
+++ scripts/app.js
@@ -18,44 +18,15 @@ $(document).ready(function(){
       if($(window).scrollTop()>1){
         changeOpacityOfNavButtons();
       }
-
       else{
         makeNavButtonOpaque();
       }
-
-      if($(window).scrollTop()>450){
-        expandPics();
-      }
-      else{
-       contractPics();
-     }
     }
 
     function changeOpacityOfNavButtons(){
-      $(".navigation").css("opacity","0.3");
+      $(".navigation").css("opacity","0.6");
       console.log("changing opacity");
     }
-
-    function expandPics(){
-      //get rid of column padding
-      //change classes from col-md-7 col-sm-7 col-xs-10 ==> col-md-12 col-sm-12 col-xs-12
-      //set .container width to 100%
-      console.log("expanding!")
-      
-      $("#paddingColumnLeft, #paddingColumnRight").hide();
-      $("#imageContainerWidthNoPadding").attr("class", "col-md-12 col-sm-12 col-xs-12 noPadding");
-      //$(".navButton").css("opacity", "0.5");
-
-    }
-
-    function contractPics(){
-      console.log("contracting");
-      $("#paddingColumnLeft, #paddingColumnRight").show();
-      $("#imageContainerWidthNoPadding").attr("class", "col-md-7 col-sm-7 col-xs-10 noPadding");
-      //$(".navButton").css("opacity", "1");
-      //reverse
-
-    };
 
     function makeNavButtonOpaque(){
       $(".navigation").css("opacity","1");
