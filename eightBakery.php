45dc5cfb2d67458e2e721c01575d36e406bc0239
Masha Vainblat <mashavainblat@gmail.com>
2016-07-27 00:55:53 -0400

    adding Eight Bakery page

---
+++ eightBakery.php
@@ -0,0 +1,87 @@
+<?php include("includes/formattop.php"); ?>
+	<title>Eight Bakery</title>
+</head>
+
+<body id="eight" class="container project">
+	<?php include("includes/navigation.php"); ?>
+
+	<main class="row projectTitleDesc">
+	
+		<section class="col-xs-1 col-sm-1 col-md-3 noPadding"></section>
+		<section class="col-xs-10 col-sm-10 col-md-6 noPadding">
+
+			<h3 class="projectTitle noMargin">Eight Bakery</h3>
+			<h4 class="projectCategory noMargin">Branding</h4>
+			<p class="projectDescription">Eight Bakery is a long island based bakery that specializes in customized cakepops and other sweet treats. The artisanal quality of Eight's product is captured in the handmade logotype that mimics the chocolate frosting swirls that can be found on any custom cake.</p>
+
+		</section>
+		<section class="col-xs-1 col-sm-1 col-md-3 noPadding"></section>
+	</main>
+
+	<main class="row projectImages">
+
+		<section class="col-md-2 hideThis"></section>
+		<section class="col-xs-12 col-md-8 noPadding images">
+
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 noPadding">
+					<img src="img/projects/eight/eight001hero.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row whiteBackground" id="sketches">
+				<div class="col-xs-6 col-sm-3 col-md-3 noPadding">
+					<img src="img/projects/eight/eight002sketch.jpg">
+				</div>
+				<div class="col-xs-6 col-sm-3 col-md-3 noPadding">
+					<img src="img/projects/eight/eight003sketch.jpg">
+				</div>
+				<div class="col-xs-6 col-sm-3 col-md-3 noPadding">
+					<img src="img/projects/eight/eight004sketch.jpg">
+				</div>
+				<div class="col-xs-6 col-sm-3 col-md-3 noPadding">
+					<img src="img/projects/eight/eight005sketch.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row whiteBackground" id="colors">
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/eight/eight006colors.jpg">
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/eight/eight007colors.jpg">
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/eight/eight008colors.jpg">
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-6 noPadding">
+					<img src="img/projects/eight/eight009colors.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 noPadding">
+					<img src="img/projects/eight/eight010cards.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+			<!-- IMG -->
+			<div class="row">
+				<div class="col-xs-12 noPadding">
+					<img src="img/projects/eight/eight011site.jpg">
+				</div>
+			</div>
+			<!-- IMG -->
+
+		</section>
+		<!-- END OF .images -->
+		<section class="col-md-2 hideThis"></section>
+
+	</main>
+
+</body>
+</html>
