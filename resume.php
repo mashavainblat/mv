934eddaa8f2082114717a292592b4a792b302942
Masha Vainblat <mashavainblat@gmail.com>
2016-11-01 23:58:32 -0400

    adding header content and continuing to style the header with bootstrap

--- resume.php
+++ resume.php
@@ -7,223 +7,109 @@
 	<?php include_once("includes/analyticstracking.php") ?>
 <?php include("includes/navigation.php"); ?>
 
-<h1>resume-update-oct2016</h1>
+	<h4>git branch: resume-update-oct2016</h4>
+
+
+
 	<main class="row">
-		<section class="col-xs-12 col-sm-12 noPadding">
+		<div class="col-xs-12 col-sm-12 noPadding">
 
-			<div class="row"  id="resumeHeaderInfo">
-				<div class="col-xs-1 col-sm-3 noMargin noPadding"> <!-- EMPTY --></div>
+			<!-- BEGIN section#headerSkillsLanguages -->
+			<section class="row" id="headerSkillsLanguages">
 
-				<div class="col-xs-10 col-sm-6 noPadding">
-					<h2>Masha S. Vainblat</h2>
-					<h2>Designer | Web Developer</h2>
+				<!-- BEGIN div#headerInfo -->
+
+				<div id="headerInfo" class="col-xs-12 col-sm-3 col-md-3" style="background-color:red">
+
+					<!-- BEGIN section#header -->
+					<section id="header">
+						<h2>Masha S. Vainblat</h2>
+						<h3>Creative Technologist</h3>
+					</section>
+					<!-- END section#header -->
+
+					<!-- BEGIN section#media -->
+					<section id="media" class="row">
+						<div class="col-xs-3 noPadding" id="media">
+							<p>linkedin</p>
+							<p>github</p>
+							<p>email</p>
+						</div>
+
+						<div class="col-xs-9 noPadding" id="mediaContact">
+							<p>mashavainblat</p>
+							<p>mashavainblat</p>
+							<p>mashavainblat@gmail.com</p>
+						</div>
+					</section>
+					<!-- BEGIN section#media -->
+
+
 				</div>
+				<!-- END div#headerInfo -->
 
-				<div class="col-xs-1 col-sm-3 noMargin noPadding"><!-- EMPTY --></div>
-			</div>
-			<!-- END OF HEADER .ROW -->
+				<!-- BEGIN div#skillsLanguages -->
+				<div id="skillsLanguages" class="col-xs-12 col-sm-9 col-md-9" style="background-color:green">
+					<div class="row">
 
+						<!-- BEGIN #skillsContainer -->
+						<div id="skillsContainer" class="col-xs-12 col-sm-4 col-md-4 noPadding">
+							<p class="subject" id="skills">skills</p>
 
+							<ul id="skillsList">
+								<li>Web Design</li>
+								<li>Interaction Design</li>
+								<li>Branding</li>
+								<li>Typography</li>
+								<li>Art Direction</li>
+								<li>Production</li>
+							</ul>
+						</div>
+						<!-- END #skillsContainer -->
 
+						<!-- BEGIN #languagesContainer -->
+						<div id="languagesContainer" class="col-xs-12 col-sm-4 col-md-4 noPadding">
+							<p class="subject" id="languages">languages</p>
 
+							<ul id="languagesList">
+								<li>Full Stack Development</li>
+								<li>HTML5/CSS</li>
+								<li>JavaScript</li>
+								<li>jQuery</li>
+								<li>Angular.js</li>
+								<li>Node.js</li>
+							</ul>
+						</div>
 
+						<div id="languagesContContainer" class="col-xs-12 col-sm-4 col-md-4 noPadding">
+							<p id="languages">languages cont</p>
 
-			<div class="row" id="skillsAndLanguages">
+							<ul id="languagesContList">
+								<li>AJAX</li>
+								<li>Wordpress</li>
+								<li>PostgreSQL</li>
+								<li>RESTful routing</li>
+								<li>MongoDB</li>
+								<li>Ruby on Rails</li>
+							</ul>
+						</div>
+						<!-- END #languagesContainer -->
 
-				<div class="col-xs-1 col-sm-3 noMargin noPadding"> <!-- EMPTY --></div>
-
-				<div class="col-xs-11 col-sm-2 noPadding" id="skills">
-					<h5 class="subject">skills</h5>
-					<ul class="noPadding bodyCopy">
-						<li>Branding</li>
-						<li>Web Design</li>
-						<li>Logo Design</li>
-						<li>Art Direction</li>
-						<li>Web Development</li>
-					</ul>
+					</div>
 				</div>
-				<!-- END OF #skills -->
-
-				<!-- BEGIN #languages -->
-				<div class="col-xs-1 noMargin noPadding desktopContainer"> <!-- EMPTY --></div>
-				<div class="col-xs-11 col-sm-2 noPadding" id="languages">
-					<h5 class="subject">languages</h5>
-					<ul class="noPadding bodyCopy">
-						<li>CSS</li>
-						<li>HTML</li>
-						<li>jQuery</li>
-						<li>Angular</li>
-						<li>javaScript</li>
-					</ul>
-				</div>
-
-				<div class="col-xs-1 noMargin noPadding desktopContainer"> <!-- EMPTY --></div>
-				<div class="col-xs-11 col-sm-2 noPadding" id="languagesCont">
-					<ul class="noPadding bodyCopy">
-						<li>Node.js</li>
-						<li>Express.js</li>
-						<li>PostgreSQL</li>
-						<li>Ruby on Rails</li>
-						<li>Fluent in Russian</li>
-					</ul>
-				</div> 
-				<!-- END OF #languages -->
-
-				<div class="col-sm-3"></div>
-				<!-- <div class="col-xs-1 col-sm-3 noMargin noPadding"></div> -->
-
-			</div>
-			<!-- END OF #skillsAndLanguages .row-->
+				<!-- END div#skillsLanguages -->
 
 
 
-
-
-			<!-- BEGIN section#education -->
-			<main id="education">
-
-				<section class="row subjectContainer">
-					<div class="col-xs-1 col-sm-3 noPadding"></div>
-					<div class="col-xs-10 col-sm-6 noPadding">
-						<h5 class="subject">education</h5>
-					</div>
-				</section>
-
-				<section class="row educationContentContainer">
-					<div class="col-xs-1 col-sm-3 noPadding"><p class="resumeYear">2016</p></div>
-					<div class="col-xs-10 col-sm-6 noPadding" id="GA">
-							<h4 class="title">General Assembly</h4>
-							<p class="bodyCopy">Full Stack Web Development</p>
-					</div>
-					<div class="col-xs-1 col-sm-3 noPadding noMargin"></div>
-				</section>
-
-				<section class="row educationContentContainer">
-					<div class="col-xs-1 col-sm-3 noPadding"><p class="resumeYear">2012 &ndash; 2016</p></div>
-					<div class="col-xs-10 col-sm-6 noPadding" id="SVA">
-							<h4 class="title">School of Visual Arts</h4>
-							<p class="bodyCopy">BFA Design</p>
-					</div>
-					<div class="col-xs-1 col-sm-3 noPadding noMargin"></div>
-				</section>
-
-			</main>
-			<!-- END OF section#education -->
-
-
-
-
-
-			<!-- BEGIN section#experience -->
-			<main id="experience">
-
-				<section class="row subjectContainer">
-					<div class="col-xs-1 col-sm-3 noPadding"></div>
-					<div class="col-xs-10 col-sm-6 noPadding">
-						<h5 class="subject">experience</h5>
-					</div>
-				</section>
-
-				<!-- VO -->
-				<section class="row experienceContentContainer">
-					<div class="col-xs-1 col-sm-3 noPadding"><p class="resumeYear">2015 &ndash; 2016</p></div>
-					<div class="col-xs-10 col-sm-6 noPadding" id="VO">
-						<h4 class="title">Visual Opinion Magazine</h4>
-						<h6 class="description">designer | art director | production manager</h6>
-						<ul class="bodyCopy">
-							<li>Achieved SPD51 custom publication nomination</li>
-							<li>Raised brand awareness by implementing social media and distribution strategy</li>
-							<li>Elevated product quality resulting in consistently sold out inventory</li>
-						</ul>
-					</div>
-					<div class="col-xs-1 col-sm-3 noPadding noMargin"></div>
-				</section>
-				<!-- end of VO -->
-
-				<!-- VAP -->
-				<section class="row experienceContentContainer">
-					<div class="col-xs-1 col-sm-3 noPadding"><p class="resumeYear">2015</p></div>
-					<div class="col-xs-10 col-sm-6 noPadding" id="VAP">
-						<h4 class="title">SVA Visual Arts Press</h4>
-						<h6 class="description">web intern | SVA in house design team</h6>
-						<ul class="bodyCopy">
-							<li>Redesgined departmental web pages that heightened site traffic</li>
-							<li>Designed wireframe templates for SVA departmental websites</li>
-						</ul>
-					</div>
-					<div class="col-xs-1 col-sm-3 noPadding noMargin"></div>
-				</section>
-				<!-- end of VAP -->
-
-				<!-- GLOW -->
-				<section class="row experienceContentContainer">
-					<div class="col-xs-1 col-sm-3 noPadding"><p class="resumeYear">2015</p></div>
-					<div class="col-xs-10 col-sm-6 noPadding" id="GLOW">
-						<h4 class="title">GLOW Interactive</h4>
-						<h6 class="description">Design intern</h6>
-						<ul class="bodyCopy">
-							<li>Created social content for clients such as HBO, SyFi, I Love NY, and TBS</li>
-							<li>Enforced maketing goals within design by following media team strategies</li>
-						</ul>
-					</div>
-					<div class="col-xs-1 col-sm-3 noPadding noMargin"></div>
-				</section>
-				<!-- end of GLOW -->
-
-				<!-- RA -->
-				<section class="row experienceContentContainer">
-					<div class="col-xs-1 col-sm-3 noPadding"><p class="resumeYear">2014 &ndash; 2015</p></div>
-					<div class="col-xs-10 col-sm-6 noPadding" id="RA">
-						<h4 class="title">SVA Resident Assistant</h4>
-						<h6 class="description">Student Supervisor</h6>
-						<ul class="bodyCopy">
-							<li>Represented SVA as a student role model by facilitating communication between students and administrative faculty</li>
-							<li>Initiated community building events to ensure building rules were followed</li>
-						</ul>
-					</div>
-					<div class="col-xs-1 col-sm-3 noPadding noMargin"></div>
-				</section>
-				<!-- end of RA -->
-
-				<!-- TA -->
-				<section class="row experienceContentContainer">
-					<div class="col-xs-1 col-sm-3 noPadding"><p class="resumeYear">2014</p></div>
-					<div class="col-xs-10 col-sm-6 noPadding" id="TA">
-						<h4 class="title">SVA Teacher Assistant</h4>
-						<h6 class="description">Precollege Program</h6>
-						<ul class="bodyCopy">
-							<li>Assisted and encouraged basic design principles to prospecive students</li>
-							<li>Curated graphic design and advertising work for gallery style presentation</li>
-						</ul>
-					</div>
-					<div class="col-xs-1 col-sm-3 noPadding noMargin"></div>
-				</section>
-				<!-- end of TA -->
-
-				<!-- SM -->
-				<section class="row experienceContentContainer">
-					<div class="col-xs-1 col-sm-3 noPadding"><p class="resumeYear">2013</p></div>
-					<div class="col-xs-10 col-sm-6 noPadding" id="SM">
-						<h4 class="title">Steve Madden. LTD</h4>
-						<h6 class="description">Madden Girl Design Intern</h6>
-						<ul class="bodyCopy">
-							<li>Produced shoes based on spec sheet measurements and tech pack design</li>
-							<li>Prepared client-product presentations and mood boards</li>
-						</ul>
-					</div>
-					<div class="col-xs-1 col-sm-3 noPadding noMargin"></div>
-				</section>
-				<!-- end of SM -->
-
-			</main>
-			<!-- END OF section#experience -->			
-
-
-
+			</section>
+			<!-- END section#headerSkillsLanguages -->
+			
 
 			
 
-		</section>
+		</div>
 	</main> <!-- END OF .ROW -->
-	</body>
-	</html>
+
+
+</body>
+</html>
