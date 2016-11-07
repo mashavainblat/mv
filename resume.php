38dcf9c5c37f2f6f96338d4fc2bb88be43ccde1d
Masha Vainblat <mashavainblat@gmail.com>
2016-11-07 18:21:42 -0500

    added all content to resume MOBILE. Will start styling now

--- resume.php
+++ resume.php
@@ -12,40 +12,6 @@
 <?php include("includes/navigation.php"); ?>
 
 
-							<!-- 
-							<ul id="skillsList">
-								<li>Web Design</li>
-								<li>Interaction Design</li>
-								<li>Branding</li>
-								<li>Typography</li>
-								<li>Art Direction</li>
-								<li>Production</li>
-							</ul> 
-							-->
-
-							<!-- 
-							<ul id="languagesList">
-								<li>Full Stack Development</li>
-								<li>HTML5/CSS</li>
-								<li>JavaScript</li>
-								<li>jQuery</li>
-								<li>Angular.js</li>
-								<li>Node.js</li>
-							</ul> 
-							-->
-						
-							<!-- 
-							<ul id="languagesContList">
-								<li>AJAX</li>
-								<li>Wordpress</li>
-								<li>PostgreSQL</li>
-								<li>RESTful routing</li>
-								<li>MongoDB</li>
-								<li>Ruby on Rails</li>
-							</ul> 
-							-->
-
-
 <!-- BEGIN section#resumeMobile -->
 <section id="resumeMobile">
 <!-- BEGIN main#headerSkillsLanguages -->
@@ -63,11 +29,20 @@
 			<div class="col-xs-1"></div><!-- EMPTY DIV -->
 		</section> 
 		<!-- END #headerContainer -->
+
+		<!-- ************************************** -->
+		<!-- ************************************** -->
 		
 		<!-- BEGIN #mediaContactContainer -->
 		<section class="row" id="mediaContactContainer">
 			<div class="col-xs-1"></div><!-- EMPTY DIV -->
-			
+		
+
+<!-- ************************************** -->
+<!-- ADD ICON IMAGES -->
+<!-- ************************************** -->
+<!-- DO LETTER SPACING  -->
+<!-- ************************************** -->
 			<div class="col-xs-2 noPadding" id="media">
 				<li>linkedin</li>
 				<li>github</li>
@@ -83,6 +58,9 @@
 			<div class="col-xs-1"></div><!-- EMPTY DIV -->
 		</section>
 		<!-- END #mediaContactContainer -->
+
+		<!-- ************************************** -->
+		<!-- ************************************** -->
 
 		<!-- BEGIN #skillsContainer -->
 		<section class="row" id="skillsContainer">
@@ -109,6 +87,9 @@
 			<div class="col-xs-1"></div><!-- EMPTY DIV -->
 		</section>
 		<!-- END #skillsContainer -->
+
+		<!-- ************************************** -->
+		<!-- ************************************** -->
 
 		<!-- BEGIN #languagesContainer -->
 		<section  class="row" id="languagesContainer">
@@ -144,7 +125,8 @@
 	</main>
 <!-- END main#headerSkillsLanguages -->
 
-
+<!-- **************************************************************************** -->
+<!-- **************************************************************************** -->
 
 <!-- BEGIN main#educationContainer -->
 	<main id="educationContainer">
@@ -152,7 +134,7 @@
 			<div class="col-xs-1"></div><!-- EMPTY DIV -->
 
 			<div class="col-xs-10 noPadding">
-				<section id="subject">
+				<section id="education">
 					<div class="subject">
 						<h3>education</h3>
 					</div>
@@ -165,27 +147,30 @@
 		<section class="row" id="educationSVA">
 			<div class="col-xs-1"></div><!-- EMPTY DIV -->
 
-			<div class="col-xs-7 noPadding" id="schoolDegree">
-				<h4 class="school">school of visual arts</h4>
-				<h5 class="degree">bfa design</h5>
+			<div class="col-xs-6 noPadding" id="schoolDegree">
+				<p class="school">school of visual arts</p>
+				<p class="degree">bfa design</p>
 			</div>
-			<div class="col-xs-3 noPadding" id="setting">
-				<p class="noMargin">New York</p>
-				<p class="noMargin">2012-2016</p>
+			<div class="col-xs-4 noPadding" id="setting">
+				<p>New York, NY</p>
+				<p>2012-2016</p>
 			</div>
 			<div class="col-xs-1"></div><!-- EMPTY DIV -->
 		</section> <!-- END #educationSVA -->
 
+		<!-- ************************************** -->
+		<!-- ************************************** -->
+
 		<section class="row" id="educationGA">
 			<div class="col-xs-1"></div><!-- EMPTY DIV -->
 
-			<div class="col-xs-7 noPadding" id="schoolDegree">
-				<h4 class="school">general assembly</h4>
-				<h5 class="degree">full stack web development</h5>
+			<div class="col-xs-6 noPadding" id="schoolDegree">
+				<p class="school">general assembly</p>
+				<p class="degree">full stack web development</p>
 			</div>
-			<div class="col-xs-3 noPadding" id="setting">
-				<p class="noMargin">New York</p>
-				<p class="noMargin">2016</p>
+			<div class="col-xs-4 noPadding" id="setting">
+				<p>New York, NY</p>
+				<p>2016</p>
 			</div>
 			<div class="col-xs-1"></div><!-- EMPTY DIV -->
 		</section> <!-- END #educationGA -->
@@ -194,16 +179,341 @@
 <!-- END main#educationContainer -->
 
 
+<!-- **************************************************************************** -->
+<!-- **************************************************************************** -->
+
 
 <!-- BEGIN main#experienceContainer -->
 	<main id="experienceContainer">
+
+		<section class="row" id="experienceSubject">
+			<div class="col-xs-1"></div><!-- EMPTY -->
+			<div class="col-xs-10 noPadding">
+				<section id="experience">
+					<div class="subject">
+						<h3>experience</h3>
+					</div>
+				</section>
+			</div>
+			<div class="col-xs-1"></div><!-- EMPTY -->
+		</section> <!-- END #experienceSubject-->
+
+	<!-- BEGIN #VOContainer -->
+		<article id="VOContainer">
+			<section class="row" id="VOTitleDescription">
+				<div class="col-xs-1"></div><!-- EMPTY -->
+
+				<div class="col-xs-6 noPadding" id="titleDescription">
+					<p class="title">visual opinion magazine</p>
+					<p class="description">designer | art director - sva student publication</p>
+				</div>
+				<div class="col-xs-4 noPadding" id="setting">
+					<p>New York, NY</p>
+					<p>2015-2016</p>
+				</div>
+
+				<div class="col-xs-1"></div><!-- EMPTY -->				
+			</section>
+
+			<section class="row" id="VObullets">
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+				<div class="col-xs-10 noPadding" id="bullets">
+					<ul>
+						<li>Achieved SPD51 custom publication nomination</li>
+						<li>Raised brand awareness by implementing social media and distribution strategy</li>
+						<li>Elevated product quality resulting in consistently sold out inventory</li>
+					</ul>
+				</div>
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+			</section>
+		</article>
+	<!-- END #VOContainer -->
+
+	<!-- ************************************** -->
+	<!-- ************************************** -->
+
+	<!-- BEGIN #VAPContainer -->
+		<article id="VAPContainer">
+			<section class="row" id="VAPTitleDescription">
+				<div class="col-xs-1"></div><!-- EMPTY -->
+
+				<div class="col-xs-6 noPadding" id="titleDescription">
+					<p class="title">SVA Visual Arts Press</p>
+					<p class="description">web design intern - SVA in house design team</p>
+				</div>
+				<div class="col-xs-4 noPadding" id="setting">
+					<p>New York, NY</p>
+					<p>2015</p>
+				</div>
+
+				<div class="col-xs-1"></div><!-- EMPTY -->				
+			</section>
+
+			<section class="row" id="VAPbullets">
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+				<div class="col-xs-10 noPadding" id="bullets">
+					<ul>
+						<li>Redesigned departmental web pages that heightened site traffic</li>
+						<li>Designed wireframe teamplates for SVA departmental websites</li>
+					</ul>
+				</div>
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+			</section>
+		</article>
+	<!-- END #VAPContainer -->
+
+	<!-- ************************************** -->
+	<!-- ************************************** -->
+
+	<!-- BEGIN #GLOWContainer -->
+		<article id="GLOWContainer">
+			<section class="row" id="GLOWTitleDescription">
+				<div class="col-xs-1"></div><!-- EMPTY -->
+
+				<div class="col-xs-6 noPadding" id="titleDescription">
+					<p class="title">Glow interactive</p>
+					<p class="description">Social Media design intern</p>
+				</div>
+				<div class="col-xs-4 noPadding" id="setting">
+					<p>New York, NY</p>
+					<p>2015</p>
+				</div>
+
+				<div class="col-xs-1"></div><!-- EMPTY -->				
+			</section>
+
+			<section class="row" id="GLOWbullets">
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+				<div class="col-xs-10 noPadding" id="bullets">
+					<ul>
+						<li>Created social content for clients such as HBO, SyFy, I Love NY, and TBS</li>
+						<li>Enforced marketing goals withing design by following media team strategies</li>
+					</ul>
+				</div>
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+			</section>
+		</article>
+	<!-- END #GLOWContainer -->
+
+	<!-- ************************************** -->
+	<!-- ************************************** -->
+
+	<!-- BEGIN #RAContainer -->
+		<article id="RAContainer">
+			<section class="row" id="RATitleDescription">
+				<div class="col-xs-1"></div><!-- EMPTY -->
+
+				<div class="col-xs-6 noPadding" id="titleDescription">
+					<p class="title">SVA Resident Assistant</p>
+					<p class="description">Student leader / role model</p>
+				</div>
+				<div class="col-xs-4 noPadding" id="setting">
+					<p>New York, NY</p>
+					<p>2014 - 2015</p>
+				</div>
+
+				<div class="col-xs-1"></div><!-- EMPTY -->				
+			</section>
+
+			<section class="row" id="RAbullets">
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+				<div class="col-xs-10 noPadding" id="bullets">
+					<ul>
+						<li>Represented SVA as a student role model by facilitating communcation between students and administrative faculty</li>
+						<li>Initiated community building events to ensure building rules were followed</li>
+					</ul>
+				</div>
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+			</section>
+		</article>	
+	<!-- END #RAContainer -->
+
+	<!-- ************************************** -->
+	<!-- ************************************** -->
+
+	<!-- BEGIN #TAContainer -->
+	<!-- END #TAContainer -->
+
+	<!-- ************************************** -->
+	<!-- ************************************** -->
+
+	<!-- BEGIN #SMContainer -->
+		<article id="SMContainer">
+			<section class="row" id="SMTitleDescription">
+				<div class="col-xs-1"></div><!-- EMPTY -->
+
+				<div class="col-xs-6 noPadding" id="titleDescription">
+					<p class="title">Steve Madden, LTD</p>
+					<p class="description">Madden girl design intern</p>
+				</div>
+				<div class="col-xs-4 noPadding" id="setting">
+					<p>Long Island City, NY</p>
+					<p>2013</p>
+				</div>
+
+				<div class="col-xs-1"></div><!-- EMPTY -->				
+			</section>
+
+			<section class="row" id="SMbullets">
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+				<div class="col-xs-10 noPadding" id="bullets">
+					<ul>
+						<li>Produced shoes based on spec sheet measurements and tech pack design</li>
+						<li>Prepared client-product presentations and mood boards</li>
+					</ul>
+				</div>
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+			</section>
+		</article>
+	<!-- END #SMContainer -->
 	</main>
 <!-- END main#experienceContainer -->
 
 
+<!-- **************************************************************************** -->
+<!-- **************************************************************************** -->
+
 
 <!-- BEGIN main#creativeProjectsContainer -->
 	<main id="creativeProjectsContainer">
+
+		<section class="row" id="creativeProjectsSubject">
+			<div class="col-xs-1"></div><!-- EMPTY -->
+			<div class="col-xs-10 noPadding">
+				<section id="experience">
+					<div class="subject">
+						<h3>creative tech projects</h3>
+					</div>
+				</section>
+			</div>
+			<div class="col-xs-1"></div><!-- EMPTY -->
+		</section> <!-- END #creativeProjectsSubject-->
+
+		<!-- BEGIN #hexContainer -->
+		<article id="hexContainer">
+			<section class="row" id="hexTitleDescription">
+				<div class="col-xs-1"></div><!-- EMPTY -->
+
+				<div class="col-xs-6 noPadding" id="titleDescription">
+					<p class="title"><a href="#">50 Shades of Hex</a></p>
+					<p class="description">designer | full stack developer</p>
+					<p class="projectTech">mean stack express web application</p>
+				</div>
+				<div class="col-xs-4 noPadding" id="setting">
+					<p>2016</p>
+				</div>
+
+				<div class="col-xs-1"></div><!-- EMPTY -->				
+			</section>
+
+			<section class="row" id="hexBullets">
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+				<div class="col-xs-10 noPadding" id="bullets">
+					<ul>
+						<li>Conceptualized, designed and programmed app prototype for designers to easity access hex color codes</li>
+					</ul>
+				</div>
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+			</section>
+		</article>
+		<!-- END #hexContainer -->
+
+		<!-- ************************************** -->
+		<!-- ************************************** -->
+
+		<!-- BEGIN #nagContainer -->
+		<article id="nagContainer">
+			<section class="row" id="nagTitleDescription">
+				<div class="col-xs-1"></div><!-- EMPTY -->
+
+				<div class="col-xs-6 noPadding" id="titleDescription">
+					<p class="title"><a href="#">Nag Me</a></p>
+					<p class="description">Designer | front end developer</p>
+					<p class="projectTech">Angular.js web application</p>
+				</div>
+				<div class="col-xs-4 noPadding" id="setting">
+					<p>2016</p>
+				</div>
+
+				<div class="col-xs-1"></div><!-- EMPTY -->				
+			</section>
+
+			<section class="row" id="nagBullets">
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+				<div class="col-xs-10 noPadding" id="bullets">
+					<ul>
+						<li>Designed and built functional "to-do" list app prototype that notifies users to complete a task</li>
+					</ul>
+				</div>
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+			</section>
+		</article>
+		<!-- END #nagContainer -->
+
+		<!-- ************************************** -->
+		<!-- ************************************** -->
+
+		<!-- BEGIN #eightContainer -->
+		<article id="eightContainer">
+			<section class="row" id="eightTitleDescription">
+				<div class="col-xs-1"></div><!-- EMPTY -->
+
+				<div class="col-xs-6 noPadding" id="titleDescription">
+					<p class="title"><a href="#">Eight Bakery</a></p>
+					<p class="description">freelance designer | art director</p>
+					<p class="projectTech">custom wordpress website</p>
+				</div>
+				<div class="col-xs-4 noPadding" id="setting">
+					<p>2016</p>
+				</div>
+
+				<div class="col-xs-1"></div><!-- EMPTY -->				
+			</section>
+
+			<section class="row" id="eightBullets">
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+				<div class="col-xs-10 noPadding" id="bullets">
+					<ul>
+						<li>conceived and developed branding and visual identity</li>
+					</ul>
+				</div>
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+			</section>
+		</article>
+		<!-- END #eightContainer -->
+
+		<!-- ************************************** -->
+		<!-- ************************************** -->
+
+		<!-- BEGIN #flowlyContainer -->
+		<article id="flowlyContainer">
+			<section class="row" id="flowlyTitleDescription">
+				<div class="col-xs-1"></div><!-- EMPTY -->
+
+				<div class="col-xs-6 noPadding" id="titleDescription">
+					<p class="title"><a href="#">Flowly</a></p>
+					<p class="description">Designer | front end developer</p>
+					<p class="projectTech">Angular.js web application</p>
+				</div>
+				<div class="col-xs-4 noPadding" id="setting">
+					<p>2016</p>
+				</div>
+
+				<div class="col-xs-1"></div><!-- EMPTY -->				
+			</section>
+
+			<section class="row" id="flowlyBullets">
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+				<div class="col-xs-10 noPadding" id="bullets">
+					<ul>
+						<li>Collaborated and developed app that teahes web development concepts</li>
+					</ul>
+				</div>
+				<div class="col-xs-1"></div> <!-- EMPTY -->
+			</section>
+		</article>
+		<!-- END #flowlyContainer -->
+
 	</main>
 <!-- END main#creativeProjectsContainer -->
 </section>
