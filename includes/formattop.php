00cec36125343f1c06b5dc3f3e620232d4f89073
Masha Vainblat <mashavainblat@gmail.com>
2017-01-04 13:24:21 -0500

    adding hotjar heatmapping tracking

--- includes/formattop.php
+++ includes/formattop.php
@@ -17,3 +17,16 @@
 
 	<script   src="https://code.jquery.com/jquery-3.0.0.min.js"   integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0="   crossorigin="anonymous"></script>
 	<script type="text/javascript" src="scripts/app.js"></script>
+
+	
+	<!-- Hotjar Tracking Code for www.mashavainblat.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:381096,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
